99bc8ae24e56315d33534cc31c407c5772afd1b2
dedlev <150597998+dedlev@users.noreply.github.com>
2024-03-20 11:36:54 +0200

    changes js 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Vimeo from "@vimeo/player";
+
+const iframe = document.querySelector('iframe');
+
+    const player = new Vimeo(iframe);
+
+    player.on('play', function(timeupdate) {
+        console.log('played the video!');
+        console.log(timeupdate);
+
+        const localStorageKey = "videoplayer-current-time"
+
+        localStorage.setItem(localStorageKey, JSON.stringify(timeupdate))
+
+        // localStorage.clear();
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+
 
