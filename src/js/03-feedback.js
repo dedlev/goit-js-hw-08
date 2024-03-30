b21c82d3c173249c177d1e9e237e741907d59744
dedlev <l.popchuk@gmail.com>
2024-03-30 15:35:44 -0500

    re-correction  of  errors

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,29 +3,28 @@ import throttle from "lodash.throttle";
 const feedbackForm = document.querySelector('.feedback-form');
 
 feedbackForm.addEventListener('input', throttle(onFormInput, 500));
-feedbackForm.addEventListener('submit', onFormSubmit)
+feedbackForm.addEventListener('submit', onFormSubmit);
 
-const STORAGE_KEY = "feedback-form-state"
+const STORAGE_KEY = "feedback-form-state";
 
-const formData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-
-console.log(formData);
+let formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) ||  {} ;
 
 Object.keys(formData).forEach((key) => {
     feedbackForm.elements[key].value = formData[key];
 })
 
-function onFormInput(evt) {
-
-    const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {} ;
-    savedData[evt.target.name] = evt.target.value;
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(savedData))
+function onFormInput(evt) {  
+    formData[evt.target.name] = evt.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 };
 
 function onFormSubmit(evt) {
+    console.log(formData);
     evt.preventDefault();
-    evt.target.reset()
+    evt.target.reset();
     localStorage.removeItem(STORAGE_KEY);
+
+    formData = {};
 };
 
 
